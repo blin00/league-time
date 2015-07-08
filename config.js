b18ffdc3c3252c35909f29f46cb243ed75cd431f
Brandon Lin <blin00@gmail.com>
2015-07-07 19:14:09 -0700

    bind to specified port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 module.exports = {
-    port: 8088,
+    port: process.env.PORT || 8088,
     key: process.env.API_KEY.trim() || fs.readFileSync('api.key').toString().trim(),
     region: 'na',
 };
