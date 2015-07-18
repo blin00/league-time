2db3baac4f12f8b88da916c6579927eda4966456
Brandon Lin <blin00@gmail.com>
2015-07-17 21:45:03 -0700

    tweak nodemon cfg, up iojs version, add mystery regex comment ;)

--- gulpfile.js
+++ gulpfile.js
@@ -61,7 +61,7 @@ gulp.task('clean', function(callback) {
 
 gulp.task('nodemon', function() {
     return gulp.src('', {read: false})
-        .pipe(shell('nodemon --ignore public --ignore src'));
+        .pipe(shell('nodemon'));
 });
 
 gulp.task('default', ['browserify', 'copy', 'mincss']);
